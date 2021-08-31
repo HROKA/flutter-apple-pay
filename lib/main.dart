83338d458287420500f5f37992d6906b7323d262
Hassan Al-Najjar <hassan.an.najjar@gmail.com>
2021-08-31 18:24:37 +0300

    Updated main screen

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
